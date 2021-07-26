b5615f4a211f3c82272df4477d1048f23002068a
Daniel Washington <danielvertigo@hotmail.com>
2021-07-26 11:29:19 -0300

    Initial Commit

---
+++ FilteroutStringsfromanArrayMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilteroutStringsfromanArrayMap
+//
+//  Created by Daniel Washington Ignacio on 26/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
